acd05570da449b90d7fb8f869d35ea45c7b34906
Stephen Vancott <thevanicotti@gmail.com>
2017-04-04 09:04:24 -0400

    added wiki model, controller, views, and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :wikis
   devise :database_authenticatable, :confirmable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
