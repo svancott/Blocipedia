112f0b9529f8b58dd434278965f5b1c63643a762
Stephen Vancott <thevanicotti@gmail.com>
2017-04-04 10:46:03 -0400

    added bootstrap to app, added create and new to wiki controller and views index show and new

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,2 +1,29 @@
 class WikisController < ApplicationController
+
+  def index
+    @wikis = Wiki.all
+  end
+
+  def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+    @wiki.user= current_user
+
+    if @wiki.save
+      flash[:notice] = "Wiki was saved."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "There was an error saving the Wiki. Please try again."
+      render :new
+    end
+  end
+
+  def show
+    @wiki = Wiki.find(params[:id])
+  end
 end
