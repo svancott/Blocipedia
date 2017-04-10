b76b4721669648ef0adc95a96027b9c36b85132f
Stephen Vancott <thevanicotti@gmail.com>
2017-04-10 13:28:09 -0400

    minimal changes

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -24,7 +24,7 @@ class WikiPolicy < ApplicationPolicy
     elsif user.role == 'premium'
       all_wikis = scope.all
       all_wikis.each do |wiki|
-        if wiki.public? || wiki.owner == user || wiki.collaborators.include?(user)
+        if wiki.public? || wiki.user == user || wiki.collaborators.include?(user)
           wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
         end
       end
