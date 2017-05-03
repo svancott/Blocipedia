81baebd1ed0c1b443dac9018e9bafc16823efd71
Stephen Vancott <thevanicotti@gmail.com>
2017-05-03 10:56:54 -0400

    rails 10 checkpoint completed

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -24,7 +24,7 @@ class WikiPolicy < ApplicationPolicy
     elsif user.role == 'premium'
       all_wikis = scope.all
       all_wikis.each do |wiki|
-        if wiki.public? || wiki.user == user || wiki.collaborators.include?(user)
+        if !wiki.private? || wiki.user == user || wiki.users.include?(user)
           wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
         end
       end
@@ -32,7 +32,7 @@ class WikiPolicy < ApplicationPolicy
       all_wikis = scope.all
       wikis = []
       all_wikis.each do |wiki|
-        if wiki.public? || wiki.collaborators.include?(user)
+        if !wiki.private? || wiki.users.include?(user)
           wikis << wiki # only show standard users public wikis and private wikis they are a collaborator on
         end
       end
